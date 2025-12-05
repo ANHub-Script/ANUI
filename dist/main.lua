a565c0e4fec91ab5cca289ea6de79d772a3330e2
Aditya Nugraha <apakau987@gmail.com>
2025-12-05 12:21:00 +0700

    build: ANUI v1.0.159

--- dist/main.lua
+++ dist/main.lua
@@ -4,7 +4,7 @@
     | |/ |/ / / _ \/ _  / /_/ // /  
     |__/|__/_/_//_/\_,_/\____/___/
     
-    v1.0.158  |  2025-12-05  |  Roblox UI Library for scripts
+    v1.0.159  |  2025-12-05  |  Roblox UI Library for scripts
     
     To view the source code, see the `src/` folder on the official GitHub repository.
     
@@ -1843,7 +1843,7 @@ New=a.load'g'.New
 return[[
 {
     "name": "ANUI",
-    "version": "1.0.158",
+    "version": "1.0.159",
     "main": "./dist/main.lua",
     "repository": "https://github.com/ANHub-Script/ANUI",
     "discord": "https://discord.gg/cy6uMRmeZ",
