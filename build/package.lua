439de662584fd05ff18374ae44c73d34f4fe3b63
Aditya Nugraha <apakau987@gmail.com>
2026-02-21 17:58:23 +0700

    build: ANUI v1.0.229

--- build/package.lua
+++ build/package.lua
@@ -3,7 +3,7 @@
 return [[
 {
     "name": "ANUI",
-    "version": "1.0.228",
+    "version": "1.0.229",
     "main": "./dist/main.lua",
     "repository": "https://github.com/ANHub-Script/ANUI",
     "discord": "https://discord.gg/cy6uMRmeZ",
